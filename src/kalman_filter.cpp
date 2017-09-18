721052603f12b8d065b6e24c07622c76f29db24e
Matthias <beef@0xdead.de>
2017-09-18 20:05:13 +0200

    Drop members 'R', 'H' from Kalman Filter
    H and R depend on the type of data that is used to update
    the state of the filter, thus they should not be a member
    of the filter itself but rather be provided for a signle update.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -7,13 +7,10 @@ KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
 
-void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in, MatrixXd &Q_in) {
   x_ = x_in;
   P_ = P_in;
   F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
   Q_ = Q_in;
 }
 
@@ -24,14 +21,14 @@ void KalmanFilter::Predict() {
   */
 }
 
-void KalmanFilter::Update(const VectorXd &z) {
+void KalmanFilter::Update(const VectorXd &z, Eigen::MatrixXd H, Eigen::MatrixXd &R) {
   /**
   TODO:
     * update the state by using Kalman Filter equations
   */
 }
 
-void KalmanFilter::UpdateEKF(const VectorXd &z) {
+void KalmanFilter::UpdateEKF(const VectorXd &z, Eigen::MatrixXd H, Eigen::MatrixXd &R) {
   /**
   TODO:
     * update the state by using Extended Kalman Filter equations
