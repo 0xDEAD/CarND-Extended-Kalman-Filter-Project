6d50b8961889176b7d0c233d6384f8845563f53b
Matthias <beef@0xdead.de>
2017-09-18 21:48:20 +0200

    Update filter with meassure from Radar

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -103,6 +103,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    ****************************************************************************/
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+    const MatrixXd Hj = Tools::CalculateJacobian(ekf_.x_);
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_, Hj, R_radar_);
+
   } else { // sensor_type_ == MeasurementPackage::LASER
     /** Meassurement from Lidar can be used directly to update the filter. */
     ekf_.Update(measurement_pack.raw_measurements_, H_laser_, R_laser_);
