56b124f7bc0a5f25ed990e668d40251e522b4707
Matthias <beef@0xdead.de>
2017-09-19 09:40:43 +0200

    Add calculation of RSME

--- src/tools.cpp
+++ src/tools.cpp
@@ -10,10 +10,25 @@ namespace Tools {
 
 VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  // ignore invalid input
+  if (estimations.size() != 4 && ground_truth.size() != 4) {
+    return rmse;
+  }
+
+  /** Calculate the root mean squares */
+  for(int i=0; i < estimations.size(); ++i) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+
+  //return the result
+  return rmse;
 }
 
 MatrixXd CalculateJacobian(const VectorXd& x_state) {
